c33fc3c222cb8959dad93c1be65fb06fab2cfc51
Alexander <alxtim10@gmail.com>
2025-05-05 12:45:09 +0700

    change loading state icon

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,12 +1,9 @@
-
 const Loading = () => {
-    return (
-        <div className="flex flex-row gap-2">
-            <div className="w-[5px] h-[5px] rounded-full bg-black animate-bounce"></div>
-            <div className="w-[5px] h-[5px] rounded-full bg-black animate-bounce [animation-delay:-.3s]"></div>
-            <div className="w-[5px] h-[5px] rounded-full bg-black animate-bounce [animation-delay:-.5s]"></div>
-        </div>
-    )
-}
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-5 h-5 border-2 border-[#30A7DA] border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
