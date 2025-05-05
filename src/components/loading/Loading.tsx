1d4664b3a769f81f87d4da72ebb1ac4419ae788a
Alexander <alxtim10@gmail.com>
2025-05-05 14:19:30 +0700

    update loading icon positioning

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -2,7 +2,7 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const Loading = () => {
   return (
-    <div className="flex items-start w-44 -ml-16">
+    <div className="flex items-start w-44 -ml-16 -mt-5">
       <DotLottieReact
         src="https://lottie.host/dbb92e2a-860b-46a5-8e77-ac47092f4106/TpUhcDnFbV.lottie"
         loop
