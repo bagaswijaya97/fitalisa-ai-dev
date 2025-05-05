5e5902d47498566477742d8b770a4ed65d3b5c51
Alexander <alxtim10@gmail.com>
2025-05-05 09:01:51 +0700

    fix chat box margin top

--- src/components/chat-box/ChatBox.tsx
+++ src/components/chat-box/ChatBox.tsx
@@ -8,7 +8,7 @@ interface ChatBoxProps {
 const ChatBox = ({ messages }: ChatBoxProps) => {
 
     return (
-        <section className="mb-44 mt-16 w-full">
+        <section className="mb-44 mt-12 w-full">
             {messages.map((message, index) => (
                 <div key={index} className={`${message.isUser ? 'justify-end' : 'justify-start'} flex items-center w-full mt-5`}>
                     <ChatBubble text={message.text} delay={10} isUser={message.isUser} isLoading={message.isLoading} image={message.image}/>
